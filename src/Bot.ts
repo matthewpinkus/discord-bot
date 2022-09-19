d4c06f877ac41f034df2020c96470ce703f531e0
mattswell1 <matthew.pinkus3@gmail.com>
2022-09-19 19:15:59 +1000

    added event listeners

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,17 @@
 import { Client } from "discord.js";
 
+import { TOKEN } from './Global'
+
+import ready from './events/ready'
+import interactionCreate from "./events/interactionCreate";
+
 console.log("I await your call, Master...")
 
 const bot = new Client({
 	intents: []
 })
 
-console.log(bot)
+ready(bot)
+interactionCreate(bot)
+
+bot.login(TOKEN)
