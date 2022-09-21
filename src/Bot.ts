bf20b29d15b2dfed19b02b81d8647922bcc028d5
mattswell1 <matthew.pinkus3@gmail.com>
2022-09-21 19:05:55 +1000

    working 'messageCreate' event handler

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,15 +2,17 @@ import { Client } from "discord.js";
 
 import { TOKEN } from './global/Global'
 
+import Intents from "./Intents";
+import { partials } from "./Partials";
+
 import interactionCreate from "./events/interactionCreate"
 import ready from './events/ready'
 import message from './events/message'
-import Intents from "./intents";
 
-const client = new Client({ intents: [ Intents ] })
+const client = new Client({ intents: [ Intents ], partials: [ partials ] })
 
 ready(client)
 interactionCreate(client)
-message(client)
+// message(client)
 
 client.login(TOKEN)
