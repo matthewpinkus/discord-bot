c7cdded187bbd0f51cddbf3757ea500939bb07a8
Matthew <18751084@student.westernsydney.edu.au>
2022-09-23 16:53:15 +1000

    /quote now fetches all cached messages

--- src/commands/Quote.ts
+++ src/commands/Quote.ts
@@ -2,11 +2,12 @@ import { CommandInteraction, Client, TextChannel, MessageManager, Collection } f
 import { CHANNEL_IDS } from "../global/Global";
 import { Command } from "../global/Command";
 
+
 async function fetchQuotes(messages: MessageManager, lastId?: string) {
     if(!messages) throw new Error(`expected messages, got ${typeof messages}`)
-    // TODO: Stop re-creating collection every time you recurse through fetchQuotes(), save them somehow
     let collection = new Collection()
 
+    // TODO: Stop re-creating collection every time you recurse through fetchQuotes(), save them somehow
     try {
         if(lastId === undefined) {
             // Fetch first 100 messages.
@@ -33,13 +34,6 @@ async function fetchQuotes(messages: MessageManager, lastId?: string) {
         })
 
         lastId = collection.lastKey() as string
-        if (lastId !== '123232732900753413'){
-            try {
-                await fetchQuotes(messages, lastId, collection)
-            } catch(e) {
-                console.log(e)
-            }
-        }
 
     } catch(e) { 
         console.log(e) 
@@ -53,14 +47,11 @@ export const Quote: Command = {
     run: async (client: Client, interaction: CommandInteraction) => {
         const channel = client.channels.cache.get(CHANNEL_IDS.QUOTES) as TextChannel
         const messages = channel.messages as MessageManager
-        // Collection of id/message pairs
         const collection = await fetchQuotes(messages)
+
         let quotes: string[] = []
 
-        for (let val of collection.values()) {
-            quotes.push(val as string) 
-        }
-        console.log(quotes.length)
+        for (let val of collection.values()) { quotes.push(val as string) }
 
         const content = `> ${quotes[Math.floor(Math.random() * quotes.length - 1)]}`
         
