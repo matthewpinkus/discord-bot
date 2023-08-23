1382b1daf6a05a1816e2252a4e810b3aec1914b4
Matthew <68981721+matthewpinkus@users.noreply.github.com>
2023-08-23 05:32:15 +0000

    added command history channel

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,34 +1,50 @@
-import { CommandInteraction, Client, Interaction } from "discord.js";
+import {
+  CommandInteraction,
+  Client,
+  Interaction,
+  TextChannel,
+} from "discord.js";
 
 import { Commands } from "../commands/Commands";
+import { CHANNEL_IDS } from "../global/Global";
 
 /**
  * Client Command: (ADD MORE INFORMATION)
  */
 export default (client: Client): void => {
-
-    client.on("interactionCreate", async (interaction: Interaction) => {
-        if (interaction.isCommand() || interaction.isContextMenuCommand()) {
-            await handleSlashCommand(client, interaction);
-        }
-    });
+  client.on("interactionCreate", async (interaction: Interaction) => {
+    if (interaction.isCommand() || interaction.isContextMenuCommand()) {
+      await handleSlashCommand(client, interaction);
+    }
+  });
 };
 
 /**
  * Find the command that was requested, (ADD MORE INFORMATION)
- * @param client 
- * @param interaction 
+ * @param client
+ * @param interaction
  */
-const handleSlashCommand = async (client: Client, interaction: CommandInteraction): Promise<void> => {
-    
-    const slashCommand = Commands.find(c => c.name === interaction.commandName);
-    if (!slashCommand) {
-        interaction.followUp({ content: "Error! Please try again or contact @kaildrai" });
-        return;
-    }   
-    
-    await interaction.deferReply()
+const handleSlashCommand = async (
+  client: Client,
+  interaction: CommandInteraction
+): Promise<void> => {
+  const slashCommand = Commands.find((c) => c.name === interaction.commandName);
+  if (!slashCommand) {
+    interaction.followUp({
+      content: "Error! Please try again or contact @kaildrai",
+    });
+    return;
+  }
 
-    console.log(`${new Date().toUTCString()}\t /${ interaction.commandName } called by ${ interaction.user.username }`); 
-    slashCommand.run(client, interaction);
-}; 
+  await interaction.deferReply();
+
+  console.log(
+    `${new Date().toUTCString()}\t /${interaction.commandName} called by ${
+      interaction.user.username
+    }`
+  );
+  (client.channels.cache.get(CHANNEL_IDS.COMMAND_HISTORY) as TextChannel).send(
+    `'*/${interaction.commandName}*' called by **${interaction.user}**`
+  );
+  slashCommand.run(client, interaction);
+};
